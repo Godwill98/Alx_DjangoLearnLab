d3c5faf1d217e140a7cf55fc4a0261df1f15c5d5
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-11-08 21:23:52 +0300

    userlogin userlogout

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView 
-from django.contrib.auth.views import LoginView,LogoutView
+from .views import list_books,UserLoginView, UserLogoutView
+
 
 
 urlpatterns = [
@@ -9,7 +9,7 @@ urlpatterns = [
     path('library_detail/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     #Authentication views
-    path('login/', LoginView.as_view, template_name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('login/', UserLoginView.as_view, template_name='login'),
+    path('logout/', UserLogoutView.as_view, template_name='logout'),
 ]
 
