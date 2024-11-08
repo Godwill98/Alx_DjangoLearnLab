1f5e262d60794a8d54e30d247afcebfcdde50480
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-11-08 21:05:18 +0300

    configure urls.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Library
-
+from django.views.generic.detail import DetailView
+from django.contrib.auth.views import LoginView, LogoutView
 # Create your views here.
 
 # Function-based view to list all books
@@ -15,3 +16,18 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+# User Login View (built-in)
+class UserLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# User logout View (built-in)
+class UserLogoutView(LogoutView):
+    template_name = 'relationship_app/login.html'
+
+
+class UserRegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('login')
