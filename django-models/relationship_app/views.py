f78ccffb4222b2ef3ddb78fc2eef0715d00cb59c
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-11-09 19:27:13 +0300

    models

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,18 +3,10 @@ from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView
-from .models import Library
+from .models import Library, Book
 from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic.detail import DetailView
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import CreateView
-from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth.decorators import permission_required
-from django.contrib.auth import login
-from django.http import HttpResponseForbidden
-from .forms import BookForm
+
 # Create your views here.
 
 # Function-based view to list all books
