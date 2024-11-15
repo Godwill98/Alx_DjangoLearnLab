109c1aaa2f392ac01abb8acda8242564c888ff5b
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-11-15 16:05:55 +0300

    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO'')

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
-"""
 
+"""
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
