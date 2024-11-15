0d044462d36ac1166f8f5c35998932590a603cce
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-11-15 15:37:20 +0300

    imported from .forms import ExampleForm

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,23 +4,23 @@ from django.http import HttpResponse, HttpResponseForbidden
 from django.contrib.auth.decorators import login_required, permission_required
 from django.db import DatabaseError
 from .models import Book
-from .forms import BookForm
+from .forms import ExampleForm
 
 
 # View to list all books
 @login_required
-@permission_required('yourapp.can_view', raise_exception=True)
+@permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     try:
         books = Book.objects.all()
-        return render(request, 'yourapp/book_list.html', {'books': books})
+        return render(request, 'bookshelf/book_list.html', {'books': books})
     except DatabaseError:
         return HttpResponse("An error occurred while fetching the books.", status=500)
 
 
 # View to create a book
 @login_required
-@permission_required('yourapp.can_create', raise_exception=True)
+@permission_required('bookshelf.can_create', raise_exception=True)
 def book_create(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
@@ -37,7 +37,7 @@ def book_create(request):
 
 # View to edit a book
 @login_required
-@permission_required('yourapp.can_edit', raise_exception=True)
+@permission_required('bookshelf.can_edit', raise_exception=True)
 def book_edit(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
@@ -50,12 +50,12 @@ def book_edit(request, pk):
                 return HttpResponse("An error occurred while updating the book.", status=500)
     else:
         form = BookForm(instance=book)
-    return render(request, 'yourapp/book_form.html', {'form': form})
+    return render(request, 'bookshelf/book_form.html', {'form': form})
 
 
 # View to delete a book
 @login_required
-@permission_required('yourapp.can_delete', raise_exception=True)
+@permission_required('bookshelf.can_delete', raise_exception=True)
 def book_delete(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
@@ -64,4 +64,4 @@ def book_delete(request, pk):
             return redirect('book_list')
         except DatabaseError:
             return HttpResponse("An error occurred while deleting the book.", status=500)
-    return render(request, 'yourapp/book_confirm_delete.html', {'book': book})
+    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
