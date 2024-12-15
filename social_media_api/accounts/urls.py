4e4e604e83cce2bc881c39360d807f2e552c5aca
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-12-15 22:32:59 +0300

    added urls

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path, include
 
-from .views import CustomUserViewSet, ProfileViewSet
+from .views import CustomUserViewSet, ProfileViewSet, RegisterView, LoginView
 
 router = DefaultRouter()
 router.register(r'users', CustomUserViewSet, basename='user')
@@ -10,6 +10,7 @@ router.register(r'profiles', ProfileViewSet, basename='profile')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('token/', obtain_auth_token),
+    path('token/', obtain_auth_token),  # Default DRF token view
+    path('register/', RegisterView.as_view(), name='register'),  # User registration
+    path('login/', LoginView.as_view(), name='login'),  # User login
 ]
-
